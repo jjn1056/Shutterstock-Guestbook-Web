c6f2101c3e289edcde3e34c65167868c9f9f0302
John Napiorkowski <jjn1056@yahoo.com>
2011-10-14 14:49:21 -0400

    added a top level file

---
+++ lib/Shutterstock/Guestbook.pm
@@ -0,0 +1,43 @@
+package Shutterstock::Guestbook;
+
+our $VERSION = '0.01';
+
+use 5.008008;
+
+1;
+
+=head1 NAME
+
+Shutterstock::Guestbook - Example of a Guestbook application
+
+=head1 SYNOPSIS
+
+  use Shutterstock::Guestbook;
+
+=head1 DESCRIPTION
+
+This is an example web application using L<Web::Simple>, L<Moo> and L<HTML::Zoom>.
+
+Written in support of tech talk
+
+To Run:
+
+1) clone repository
+2) install dependencies ("cpanm --installdeps ." or similar)
+3) start: "plackup lib/Shutterstock/Guestbook/Web.pm"
+
+Then point your browser to http://localhost:5000
+
+=head1 AUTHOR
+
+John Napiorkowski C< <<jjnapiork@cpan.org>> >
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011, John Napiorkowski
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
